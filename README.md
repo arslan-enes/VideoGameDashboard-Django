e9828be6a6ee6b1e56bc2912fcf28f717abeb999
arslan-enes <arslanenes515@gmail.com>
2023-06-16 14:00:40 +0300

    create: VideoGame model

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 3. ```pip install django```
 4. ```django-admin startproject videogamedashboard```
 5. ```cd videogamedashboard```
+6. ```python manage.py startapp dashboard```
+7. create VideoGame model in dashboard/models.py
+8. ```python manage.py makemigrations```
+9. ```python manage.py migrate```
+10. ```python manage.py createsuperuser```
+11. update dashboard/admin.py
--- videogamedashboard/dashboard/models.py
+++ videogamedashboard/dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class VideoGame(models.Model):
+    title = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    release_date = models.DateField()
+    metascore = models.FloatField()
+    user_score = models.FloatField()
+    metascore_count = models.IntegerField()
+    user_rating_count = models.CharField(max_length=255)
+    platform = models.CharField(max_length=255)
+    genres = models.CharField(max_length=255)
+    summary = models.TextField()
+    critic_reviews = models.TextField()
+
+    def __str__(self):
+        return self.title
+
