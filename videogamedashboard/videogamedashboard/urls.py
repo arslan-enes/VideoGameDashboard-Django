3d488a835907cb713161971483f28c7079ea91cb
arslan-enes <arslanenes515@gmail.com>
2023-06-18 00:50:50 +0300

    add: dashboard view

--- videogamedashboard/videogamedashboard/urls.py
+++ videogamedashboard/videogamedashboard/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from dashboard import views as dashboard_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
 ]
