ae472bf332364887e54338d9f4398e591d6d330d
arslan-enes <arslanenes515@gmail.com>
2023-06-19 00:35:02 +0300

    add: dashboard/scatter

--- videogamedashboard/dashboard/urls.py
+++ videogamedashboard/dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('bar', views.bar, name='bar'),
+    path('scatter_data', views.scatter_data, name='scatter_data'),
+    path('scatter', views.scatter, name='scatter'),
 ]
--- videogamedashboard/dashboard/views.py
+++ videogamedashboard/dashboard/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import VideoGame
 import pandas as pd
 
+games = VideoGame.objects.all()
+games = pd.DataFrame(games.values())
+
 def bar(request):
 
-    games = VideoGame.objects.all()
-
-    games = pd.DataFrame(games.values())
     
     genres = ["Action", "Adventure", "Puzzle", "Racing", "Role-Playing", "Platformer", "Shooter", "First-Person", "Open-World"]
 
@@ -15,9 +16,36 @@ def bar(request):
     for genre in genres:
         genre_counts[genre] = games[games["genres"].str.contains(genre)].shape[0]
 
-    print(games.genres.value_counts())
-
-
     return render(request, "dashboard/bar.html", {"genre_counts_dict":  genre_counts,
                                                   "genre_counts":       list(genre_counts.values()),
                                                   "genres":        list(genre_counts.keys())})
+
+def scatter_data(request):
+
+    data = games.sort_values(by=["metascore", "user_score"], ascending=False)[["title", "metascore", "user_score"]].dropna()
+    data = data.sample(50)
+    label = data["title"].to_list()
+    data = data.drop(columns=["title"])
+    data["user_score"] = data["user_score"] * 10
+    data = data.rename(columns={"metascore": "x", "user_score": "y"})
+    data = data.to_dict(orient="records")
+
+    labels = label
+    datasets = [
+        {
+            'label': 'PC Games',
+            'data': data,
+            'backgroundColor': 'rgba(255, 99, 132, 0.5)'  # Set color if needed
+        }
+    ]
+
+    data = {
+        'labels': labels,
+        'datasets': datasets
+    }
+
+    return JsonResponse(data)
+
+
+def scatter(request):
+    return render(request, "dashboard/scatter.html")
