6e1a44e24bd9ee23bc3787b19f44b678997b5cf2
arslan-enes <arslanenes515@gmail.com>
2023-06-18 03:33:16 +0300

    create: page structure and first graph into /bar

--- videogamedashboard/dashboard/views.py
+++ videogamedashboard/dashboard/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from .models import VideoGame
+import pandas as pd
 
-def index(request):
-    return render(request, "index.html")
+def bar(request):
+
+    games = VideoGame.objects.all()
+
+    games = pd.DataFrame(games.values())
+    
+    selected_genres = ["Action", "Adventure", "Puzzle", "Racing", "Role-Playing"]
+
+    genre_counts = []
+
+    for genre in selected_genres:
+        genre_counts.append(games[games["genres"].str.contains(genre)].shape[0])
+
+    print(selected_genres)
+    print(genre_counts)
+
+
+    return render(request, "dashboard/bar.html", {"genres": selected_genres, "genre_counts": genre_counts})
